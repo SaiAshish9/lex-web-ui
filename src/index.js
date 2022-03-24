2c38dcacd0d0a34ba2890553c5717289a03070bd
saiashish9 <saiashish3760@gmail.com>
2022-03-24 15:32:11 +0530

    updated home screen

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyles from "styles";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
